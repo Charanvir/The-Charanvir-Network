e6aaa69917ad295c41c54921e627c7b6c216b914
Charanvir <charanvir123@gmail.com>
2022-06-20 04:19:15 -0400

    created User model (untested)

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema({
+    user: {
+        type: String,
+        unique: true,
+        required: 'You need to provide a username!',
+        trim: true
+    },
+    email: {
+        type: String,
+        required: "You must provide an email!",
+        unique: true,
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please provide a valud email!']
+    },
+    thoughts: [{
+        // array of _id values referencing the Thought model
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types, ObjectId,
+        ref: "User"
+    }]
+    // array of _id values referncing the User model (self-reference)
+
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// virtual called friendCount that retrives the length of the user's friend array field on query
+UserSchema.virtual('fiendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
