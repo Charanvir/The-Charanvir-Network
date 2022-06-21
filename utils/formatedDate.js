e4e5eb166a7bc4d73e4d9d7dacc4ab0f045bcd8e
Charanvir <charanvir123@gmail.com>
2022-06-21 19:29:27 -0400

    fixed date format bug

--- utils/formatedDate.js
+++ utils/formatedDate.js
@@ -1,5 +1,5 @@
 function formatedDate(date) {
-    date.toDateString();
+    return `${new Date(date).getFullYear()}/${new Date(date).getMonth() + 1}/${new Date(date).getDate()} @ ${new Date(date).getHours()}:${new Date(date).getMinutes()}`;
 }
 
 module.exports = formatedDate
